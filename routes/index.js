05878dd506994a95c67d9f99dceba67c3833d0e4
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-09-10 13:10:11 -0500

    quotes starting state

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,7 @@ const client = new Client({
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('routing in index');
-  res.render('locator', {success: false});
-});
-
-/* form handling */
-router.post('/leadform', async function (req, res, next) {
-  console.log('routing on post form');
-  console.log(req.body);
-  await client.connect();
-  const result = await client.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
-  console.log(result);
-  res.render('locator', { success: true });
+  res.render('home', {success: false});
 });
 
 module.exports = router;
